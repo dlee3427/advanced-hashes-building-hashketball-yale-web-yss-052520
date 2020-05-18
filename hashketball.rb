93dbe977d7aef10e964f70027cad425b0d274e88
David Lee <david.lee.sl2634@yale.edu>
2020-05-18 21:56:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -29,17 +29,17 @@ game_hash = {
       }
       {
         :player_name => "Brook Lopez"
-        :number =>
-        :shoe => 
-        :points => 
-        :rebounds =>
-        :assists => 
-        :steals =>
-        :blocks => 
-        :slam_dunks => 
+        :number => 11
+        :shoe => 17
+        :points => 17
+        :rebounds => 19
+        :assists => 10
+        :steals => 3
+        :blocks => 1 
+        :slam_dunks => 15
       }
       {
-        :player_name =>
+        :player_name =>"Mason Plumlee"
         :number =>
         :shoe => 
         :points => 
