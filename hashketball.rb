251e1508a4b1b74cc619bc9004cb86e180487d89
David Lee <david.lee.sl2634@yale.edu>
2020-05-18 22:15:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,5 @@
 # Write your code here!
-require 'pry'
+def game_hash 
 game_hash = {
   :home => { 
     team_name:"Brooklyn Nets",
@@ -125,5 +125,6 @@ game_hash = {
       ]
   }
 }
-
+return game_hash
+end 
   
