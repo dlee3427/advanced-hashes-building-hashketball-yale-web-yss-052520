0e5a5bc642d13b0c8338cf22530a1d72e507c1e4
David Lee <david.lee.sl2634@yale.edu>
2020-05-18 22:12:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -108,19 +108,19 @@ game_hash = {
         :points => 33,
         :rebounds => 3,
         :assists => 2,
-        :steals => 1 
-        :blocks => 1
+        :steals => 1,
+        :blocks => 1,
         :slam_dunks => 0
       }
       {
-        :player_name => "Kemba Walker"
-        :number => 33
-        :shoe => 15
-        :points => 6
-        :rebounds => 12
-        :assists => 12 
-        :steals => 7
-        :blocks => 5 
+        :player_name => "Kemba Walker",
+        :number => 33,
+        :shoe => 15,
+        :points => 6,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 7,
+        :blocks => 5, 
         :slam_dunks => 12
       }
       ]
