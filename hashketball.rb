4bcb465ccd9545fad052f0f0dc60346d4160dc96
David Lee <david.lee.sl2634@yale.edu>
2020-05-18 21:59:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -40,31 +40,27 @@ game_hash = {
       }
       {
         :player_name =>"Mason Plumlee"
-        :number =>
-        :shoe => 
-        :points => 
-        :rebounds =>
-        :assists => 
-        :steals =>
-        :blocks => 
-        :slam_dunks => 
+        :number => 1
+        :shoe => 19
+        :points => 26
+        :rebounds =>11
+        :assists => 6
+        :steals => 3
+        :blocks => 8
+        :slam_dunks => 5
       }
       {
-        :player_name =>
-        :number =>
-        :shoe => 
-        :points => 
-        :rebounds =>
-        :assists => 
-        :steals =>
-        :blocks => 
-        :slam_dunks =>  
-      
-        
-        
+        :player_name => "Jason Terry"
+        :number => 31
+        :shoe => 15
+        :points => 19
+        :rebounds => 2
+        :assists => 2
+        :steals => 4
+        :blocks => 11
+        :slam_dunks => 1
       }
       ]
-    
   }
   
   
@@ -73,18 +69,18 @@ game_hash = {
     colors: ["Turquoise", "Purple"]
     players: [
       {
-        :player_name =>
-        :number =>
-        :shoe => 
-        :points => 
-        :rebounds =>
-        :assists => 
-        :steals =>
-        :blocks => 
-        :slam_dunks => 
+        :player_name => "Jeff Adrien"
+        :number => 4
+        :shoe => 18
+        :points => 10
+        :rebounds => 1
+        :assists => 1
+        :steals => 2
+        :blocks => 7
+        :slam_dunks => 2
       }
       {
-        :player_name =>
+        :player_name => 
         :number =>
         :shoe => 
         :points => 
